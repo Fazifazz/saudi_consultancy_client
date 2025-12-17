0e09926309d5103817e65632aa51799586484a8a
MUHAMMED FAYAZ T S <139971477+MuhammedFayazTS@users.noreply.github.com>
2025-12-17 10:45:12 +0530

    chore: add shadcn ui (#2)
    * chore: add shadcn ui
    
    * fix: workflow

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-import Image from 'next/image';
+import { ComponentExample } from '@/components/component-example';
 
-export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1 className='text-xl'>Hello World</h1>
-    </div>
-  );
+export default function Page() {
+  return <ComponentExample />;
 }
