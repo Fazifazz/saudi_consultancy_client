c7ce1a16d1c6dac4099675d86a7bdb6437747023
MUHAMMED FAYAZ T S <139971477+MuhammedFayazTS@users.noreply.github.com>
2025-12-21 12:25:41 +0530

    feat: add authentication and authorization (#4)
    * feat: add authentication and authorization
    
    * format

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,6 @@
+import { LoginForm } from '@/components/forms/LoginForm';
+import { GalleryVerticalEnd } from 'lucide-react';
+
+export default function LoginPage() {
+  return <LoginForm />;
+}
---
+++ context/UserContext.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { createContext, useContext, ReactNode } from 'react';
+
+const UserContext = createContext<any>(null);
+
+export const UserProvider = ({ user, children }: { user: any; children: ReactNode }) => {
+  return <UserContext.Provider value={user}>{children}</UserContext.Provider>;
+};
+
+export const useUser = () => useContext(UserContext);
