e08486afc1289a824ba0c7848c775a2521bf5c7f
MUHAMMED FAYAZ T S <muhammedfayazts01@gmail.com>
2025-12-23 20:04:52 +0530

    feat: add dashboard layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,20 @@
 import { UserProvider } from '@/context/UserContext';
 import { cookies } from 'next/headers';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+import { AppSidebar } from '@/components/sidebar/app-sidebar';
 
 async function getMe() {
   const cookieStore = await cookies();
@@ -24,5 +39,35 @@ export default async function AuthLayout({
   children: React.ReactNode;
 }>) {
   const user = await getMe();
-  return <UserProvider user={user}>{children}</UserProvider>;
+  return <UserProvider user={user}>
+
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator
+            orientation="vertical"
+            className="mr-2 data-[orientation=vertical]:h-4"
+          />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem className="hidden md:block">
+                <BreadcrumbLink href="#">
+                  Building Your Application
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator className="hidden md:block" />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </header>
+        <main>
+          {children}
+        </main>
+      </SidebarInset>
+    </SidebarProvider>
+  </UserProvider>;
 }
