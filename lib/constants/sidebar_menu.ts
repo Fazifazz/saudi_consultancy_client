c7233f863e57ee3abdec34e16a99f853535fd09b
MUHAMMED FAYAZ T S <muhammedfayazts01@gmail.com>
2025-12-23 20:24:29 +0530

    format

--- lib/constants/sidebar_menu.ts
+++ lib/constants/sidebar_menu.ts
@@ -1,24 +1,21 @@
-import {
-    LucideIcon,
-    Users
-} from "lucide-react"
+import { LucideIcon, Users } from 'lucide-react';
 
 export type SidebarMenuItem = {
+  title: string;
+  url: string;
+  icon: LucideIcon;
+  items?: {
     title: string;
     url: string;
-    icon: LucideIcon;
-    items?: {
-        title: string;
-        url: string;
-    }[];
-    isActive?: boolean;
-}
+  }[];
+  isActive?: boolean;
+};
 
 export const SIDEBAR_MENU: SidebarMenuItem[] = [
-    {
-        title: "Customer",
-        url: "/customer",
-        icon: Users,
-        isActive: true,
-    },
-]
+  {
+    title: 'Customer',
+    url: '/customer',
+    icon: Users,
+    isActive: true,
+  },
+];
