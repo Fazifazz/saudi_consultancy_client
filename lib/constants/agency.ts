ed923608ffa7e5f3d915c1a4937b00eab07a2ac5
MUHAMMED FAYAZ T S <muhammedfayazts01@gmail.com>
2026-01-13 10:18:45 +0530

    chore: update agency list

--- lib/constants/agency.ts
+++ lib/constants/agency.ts
@@ -1,1 +1,8 @@
-export const AGENCIES = [{ value: 'MANJERI', label: 'Manjeri' }];
+export const AGENCIES = [
+  { value: 'KALPPATTA', label: 'Kalppatta' },
+  { value: 'COCHIN', label: 'Cochin' },
+  { value: 'ANGAMALI', label: 'Angamali' },
+  { value: 'BANGLORE', label: 'Bangalore' },
+  { value: 'MUMBAI', label: 'Mumbai' },
+  { value: 'OTHERS', label: 'Others' },
+];
